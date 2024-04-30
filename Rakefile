970d1e36b712b6cdd897126c279fe20af02efe17
tsaiadrian <tsaiadrian414@gmail.com>
2024-04-30 21:49:23 +0800

    merge

--- Rakefile
+++ Rakefile
@@ -13,7 +13,7 @@ end
 desc 'Test all the specs'
 Rake::TestTask.new(:spec) do |t|
   # t.pattern = 'spec/**/*_spec.rb'
-  t.pattern = 'spec/unit/schedules_spec.rb'
+  t.pattern = 'spec/**/schedules_spec.rb'
   t.warning = false
 end
 
--- spec/unit/schedules_spec.rb
+++ spec/unit/schedules_spec.rb
@@ -7,25 +7,26 @@ describe 'Test Schedule Handling' do
     wipe_database
 
     DATA[:schedules].each do |schedule_data|
-      No2Date::Schedule.create(schedule_data)
+      No2Date::Meeting.create(schedule_data)
     end
   end
 
   it 'HAPPY: should retrieve correct data from database' do
     sched_data = DATA[:schedules][1]
-    meet = No2Date::Schedule.first
+    puts "schedules_spec.rb #{sched_data}"
+    meet = No2Date::Meeting.first
     new_sched = meet.add_schedule(sched_data)
 
     sched = No2Date::Schedule.find(id: new_sched.id)
-    _(sched.title).must_equal sched_data[:title]
-    _(sched.description).must_equal sched_data[:description]
-    _(sched.location).must_equal sched_data[:location]
-    _(sched.start_date).must_equal sched_data[:start_date]
-    _(sched.start_datetime).must_equal sched_data[:start_datetime]
-    _(sched.end_date).must_equal sched_data[:end_date]
-    _(sched.end_datetime).must_equal sched_data[:end_datetime]
-    _(sched.is_regular).must_equal sched_data[:is_regular]
-    _(sched.is_flexible).must_equal sched_data[:is_flexible]
+    _(sched.title).must_equal sched_data['title']
+    _(sched.description).must_equal sched_data['description']
+    _(sched.location).must_equal sched_data['location']
+    _(sched.start_date).must_equal sched_data['start_date']
+    _(sched.start_datetime).must_equal sched_data['start_datetime']
+    _(sched.end_date).must_equal sched_data['end_date']
+    _(sched.end_datetime).must_equal sched_data['end_datetime']
+    _(sched.is_regular).must_equal sched_data['is_regular']
+    _(sched.is_flexible).must_equal sched_data['is_flexible']
   end
 
   # it 'SECURITY: should not use deterministic integers' do
