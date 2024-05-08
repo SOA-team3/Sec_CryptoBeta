c4571fb52347921869b3209a6374973f1372c568
楊誼萱 <ella5898g5@gmail.com>
2024-05-08 22:41:56 +0800

    update app.rb sched and meet routing

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -48,74 +48,108 @@ module No2Date
           end
         end
 
-      
-        routing.on 'meetings' do
-          @meet_route = "#{@api_root}/meetings"
+        # if the "account" is directing to "meetings"
+        @api_root = 'api/v1'
+        routing.on @api_root do
+          routing.on 'meetings' do
+            @meet_route = "#{@api_root}/meetings"
 
-          routing.on String do |meet_id|
-            routing.on 'schedules' do
-              @sched_route = "#{@api_root}/meetings/#{meet_id}/schedules"
-              # GET api/v1/meetings/[meet_id]/schedules/[sched_id]
-              routing.get String do |sched_id|
-                sched = Schedule.where(meeting_id: meet_id, id: sched_id).first
-                sched ? sched.to_json : raise('Schedule not found')
+            routing.on String do |meet_id|
+              # GET api/v1/meetings/[ID]
+              routing.get do
+                meet = Meeting.first(id: meet_id)
+                meet ? meet.to_json : raise('Meeting not found')
               rescue StandardError => e
                 routing.halt 404, { message: e.message }.to_json
               end
 
-              # GET api/v1/meetings/[meet_id]/schedules
-              routing.get do
-                output = { data: Meeting.first(id: meet_id).schedules }
-                JSON.pretty_generate(output)
-              rescue StandardError
-                routing.halt 404, message: 'Could not find schedules'
-              end
+              # Add 2nd routing into the relation of meeting
+              # routing.on 'schedules' do
+              #   @sched_route = "#{@api_root}/meetings/#{meet_id}/schedules"
+              #   # GET api/v1/meetings/[meet_id]/schedules/[sched_id]
+              #   routing.get String do |sched_id|
+              #     sched = Schedule.where(meeting_id: meet_id, id: sched_id).first
+              #     sched ? sched.to_json : raise('Schedule not found')
+              #   rescue StandardError => e
+              #     routing.halt 404, { message: e.message }.to_json
+              #   end
 
-              # POST api/v1/meetings/[ID]/schedules
-              # Mass assign attributes to service object
-              routing.post do
-                new_data = JSON.parse(routing.body.read)
-                meet = Meeting.first(id: meet_id)
-                new_sched = meet.add_schedule(new_data)
-                raise 'Could not save schedule' unless new_sched
+              #   # GET api/v1/meetings/[meet_id]/schedules
+              #   routing.get do
+              #     output = { data: Meeting.first(id: meet_id).schedules }
+              #     JSON.pretty_generate(output)
+              #   rescue StandardError
+              #     routing.halt 404, message: 'Could not find schedules'
+              #   end
 
-                response.status = 201
-                response['Location'] = "#{@sched_route}/#{new_sched.id}"
-                { message: 'Schedule saved', data: new_sched }.to_json
-              rescue Sequel::MassAssignmentRestriction
-                Api.logger.warn "MASS-ASSIGNMENT: #{new_data.keys}"
-                routing.halt 400, { message: 'Illegal Attributes' }.to_json
-              rescue StandardError => e
-                routing.halt 500, { message: e.message }.to_json
+              #   # POST api/v1/meetings/[ID]/schedules
+              #   routing.post do
+              #     new_data = JSON.parse(routing.body.read)
+              #     meet = Meeting.first(id: meet_id)
+              #     new_sched = meet.add_schedule(new_data)
+              #     raise 'Could not save schedule' unless new_sched
+
+              #     response.status = 201
+              #     response['Location'] = "#{@sched_route}/#{new_sched.id}"
+              #     { message: 'Schedule saved', data: new_sched }.to_json
+              #   rescue Sequel::MassAssignmentRestriction
+              #     Api.logger.warn "MASS-ASSIGNMENT: #{new_data.keys}"
+              #     routing.halt 400, { message: 'Illegal Attributes' }.to_json
+              #   rescue StandardError => e
+              #     routing.halt 500, { message: e.message }.to_json
+              #   end
               end
             end
 
-            # GET api/v1/meetings/[ID]
+            # GET api/v1/meetings
             routing.get do
-              meet = Meeting.first(id: meet_id)
-              meet ? meet.to_json : raise('Meeting not found')
+              output = { data: Meeting.all }
+              JSON.pretty_generate(output)
+            rescue StandardError
+              routing.halt 404, { message: 'Could not find meetings' }.to_json
+            end
+
+            # POST api/v1/meetings
+            routing.post do
+              new_data = JSON.parse(routing.body.read)
+              new_meet = Meeting.new(new_data)
+              raise('Could not save meeting') unless new_meet.save
+
+              response.status = 201
+              response['Location'] = "#{@meet_route}/#{new_meet.id}"
+              { message: 'Meeting saved', data: new_meet }.to_json
+            rescue Sequel::MassAssignmentRestriction
+              Api.logger.warn "MASS-ASSIGNMENT: #{new_data.keys}"
+              routing.halt 400, { message: 'Illegal Attributes' }.to_json
             rescue StandardError => e
-              routing.halt 404, { message: e.message }.to_json
+              Api.logger.error "UNKOWN ERROR: #{e.message}"
+              routing.halt 500, { message: 'Unknown server error' }.to_json
             end
           end
 
-          # GET api/v1/meetings
+        end
+
+        # if the "account" is directing to "schedules"
+        routing.on 'schedules' do
+          @sched_route = "#{@api_root}/schedules"
+
+          # GET api/v1/schedules
           routing.get do
-            output = { data: Meeting.all }
+            output = { data: Schedule.all }
             JSON.pretty_generate(output)
           rescue StandardError
-            routing.halt 404, { message: 'Could not find meetings' }.to_json
+            routing.halt 404, { message: 'Could not find schedules' }.to_json
           end
 
-          # POST api/v1/meetings
+          # POST api/v1/schedules
           routing.post do
             new_data = JSON.parse(routing.body.read)
-            new_meet = Meeting.new(new_data)
-            raise('Could not save meeting') unless new_meet.save
+            new_sched = Schedule.new(new_data)
+            raise('Could not save schedules') unless new_sched.save
 
             response.status = 201
-            response['Location'] = "#{@meet_route}/#{new_meet.id}"
-            { message: 'Meeting saved', data: new_meet }.to_json
+            response['Location'] = "#{@sched_route}/#{new_sched.id}"
+            { message: 'Schedule saved', data: new_sched }.to_json
           rescue Sequel::MassAssignmentRestriction
             Api.logger.warn "MASS-ASSIGNMENT: #{new_data.keys}"
             routing.halt 400, { message: 'Illegal Attributes' }.to_json
@@ -124,6 +158,7 @@ module No2Date
             routing.halt 500, { message: 'Unknown server error' }.to_json
           end
         end
+
       end
     end
   end
