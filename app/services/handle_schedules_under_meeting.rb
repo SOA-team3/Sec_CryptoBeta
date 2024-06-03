d63f6b74cfcfcf5c3f23d5ad5b1a56feac94b7f3
tsaiadrian <tsaiadrian414@gmail.com>
2024-06-03 10:16:01 +0800

    service object modify

---
+++ app/services/handle_schedules_under_meeting.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'json'
+
+module No2Date
+  # get all schedules of all participants in a meeting
+  class HandleSchedulesUnderMeeting
+    def initialize(account, meeting)
+      @account = account 
+      @meeting = meeting
+      @all_schedules = []
+    end
+
+    def find_schedules(@meeting)
+      # Retrieve schedules for the meeting owner
+      @meeting.owner.schedules.each do |schedule|
+        schedule_data = schedule.to_json
+
+        # Add the schedule data to the array
+        @all_schedules << schedule_data
+      end
+
+      @meeting.attenders.each do |attender|
+        # Retrieve schedules for each attender
+        attender.schedules.each do |schedule|
+          schedule_data = schedule.to_json
+
+          # Add the schedule data to the array
+          @all_schedules << schedule_data
+        end
+      end
+
+      # Return collected schedules
+      @all_schedules
+    end
+
+    # SHOULD HAVE METHOD TO SEND TO VALUE TO CALCULATE AVAILABLE TIME
+  end
+end
