bd6083eb01493d994345fced527d85d2bc9c31b8
tsaiadrian <tsaiadrian414@gmail.com>
2024-06-17 23:36:22 +0800

    google sso fix

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ ruby File.read('.ruby-version').strip
 gem 'json'
 gem 'puma', '~>6.2'
 gem 'roda', '~>3.54'
+gem 'httparty'
+gem 'sinatra'
 
 # Configuration
 gem 'figaro', '~>1.2'
--- app/services/authorize_sso.rb
+++ app/services/authorize_sso.rb
@@ -1,33 +1,45 @@
 # frozen_string_literal: true
 
 require 'http'
+require 'uri'
+require 'sinatra'
+require 'httparty'
+# require_relative '../models/google_account.rb'
+
 
 module No2Date
   # Find or create an SsoAccount based on Google code
   class AuthorizeSso
-    def call(access_token)
-      goog_account = get_goog_account(access_token)
+    def call(access_token, id_token)
+      goog_account = get_goog_account(access_token, id_token)
       sso_account = find_or_create_sso_account(goog_account)
 
       account_and_token(sso_account)
     end
 
-    def get_goog_account(access_token)
-      goog_response = HTTP.headers(
-        user_agent: 'No2Date',
-        authorization: "token #{access_token}",
-        accept: 'application/json'
-      ).get(ENV['goog_account_URL'])
+    def get_goog_account(access_token, id_token)
+      puts "authorize_sso.rb, get_goog_account"
 
-      raise unless goog_response.status == 200
+      goog_response = HTTParty.get(
+        "https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=#{access_token}",
+        headers: { "Authorization" => "Bearer #{id_token}" }
+      )
 
-      account = GoogleAccount.new(JSON.parse(goog_response))
+      puts "authorize_sso.rb, get_goog_account: goog_response: #{goog_response}"
+      
+      # raise unless goog_response.status == 200
+
+      account_json = JSON.parse(goog_response.body)
+
+      puts "authorize_sso.rb, get_goog_account: account_json: #{account_json}"
+      
+      account = GoogleAccount.new(account_json)
       { username: account.username, email: account.email }
     end
 
     def find_or_create_sso_account(account_data)
       Account.first(email: account_data[:email]) ||
-        Account.create_goog_account(account_data)
+        Account.create_google_account(account_data)
     end
 
     def account_and_token(account)
